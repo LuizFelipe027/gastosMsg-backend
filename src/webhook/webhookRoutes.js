bd6b0ba087c1093fbf01485372db1f0e50e77a65
Luiz Felipe Ribeiro Paixão <66446100+LuizFelipe027@users.noreply.github.com>
2025-06-09 16:56:10 -0300

    get de verificacao do meta

--- src/webhook/webhookRoutes.js
+++ src/webhook/webhookRoutes.js
@@ -2,6 +2,22 @@ const express = require('express')
 const router = express.Router()
 const webhook = require('./webhookService')
 
+const VERIFY_TOKEN = 'gastosmsg!@'
+
+router.get('/webhook/gastos', (req, res) => {
+  const mode = req.query['hub.mode']
+  const token = req.query['hub.verify_token']
+  const challenge = req.query['hub.challenge']
+
+  if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+    console.log('Webhook verificado com sucesso!')
+    res.status(200).send(challenge)
+  } else {
+    console.log('Verificação falhou.')
+    res.sendStatus(403)
+  }
+})
+
 router.post('/webhook/gastos', webhook.processWebhook)
 
 module.exports = router
